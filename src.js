e8f1ea23d79975a0eb48e4f897e13bbeecdc94ed
Rihan-saifi46 <kharihan786k@gmail.com>
2025-03-28 00:30:00 +0530

    first

---
+++ src.js
@@ -0,0 +1,13 @@
+VANTA.RINGS({
+    el: ".landing",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x203,
+    color: 0x410c79
+  })
+  
